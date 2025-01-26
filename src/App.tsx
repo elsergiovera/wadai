c1fd141bb3143cd2ed7f09da8d8f510e53a962b5
elsergio <elsergio@gmail.com>
2025-01-26 15:27:10 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './style/App.css'
+
+const App = () => {
+  return (
+    <div className='grid place-content-center min-h-screen bg-gray-50 py-5 overflow-hidden'>
+      <div className='min-w-[300px] shadow-lg rounded-xl overflow-hidden'>
+        <div className='min-h-[50px] place-content-center bg-red-500 text-center text-white py-1 px-2 text-3xl font-semibold'>JUNE</div>
+        <div className='min-h-[200px] place-content-center bg-white text-center py-2 px-2 text-9xl font-light'>19</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
