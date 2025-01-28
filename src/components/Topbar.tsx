30e0611fecad3c2e8eed780191bd9f0ec1339d46
elsergio <elsergio@gmail.com>
2025-01-28 20:05:29 -0300

    fix text-center

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -9,7 +9,7 @@ const Topbar: React.FC<TopbarProps> = ({ handleToggleMenu }) => {
    return (
       <div className='w-full min-h-[50px] grid grid-cols-3 bg-red-500 text-white'>
          <div className='place-content-center pl-2'><AlignJustify onClick={handleToggleMenu} /></div>
-         <div className='place-content-center text-2xl'><span className='font-custom'>{VITE_ENV_APP_NAME}</span></div>
+         <div className='place-content-center text-center text-2xl'><span className='font-custom'>{VITE_ENV_APP_NAME}</span></div>
          <div></div>
       </div>
    )
